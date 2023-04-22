5cd0c8ba2b1255ee560ae9e8b7488c269d8a5fe8
NewtonLC <55558755+NewtonLC@users.noreply.github.com>
2023-04-22 22:36:55 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+'''
+OBJECTIVES:
+
+Create a system that takes in a date, time, and user message.
+When the date and time are met, make a sound and display the user message.
+
+Cretae a system that takes in a set time and a user message. Wait that many seconds,
+then make a sound and display the user message.
+
+
+
+'''
